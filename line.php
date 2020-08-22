62b2222adb58230f382d8c967927c307f36b040d
popeye-api <70052083+popeye-api@users.noreply.github.com>
2020-08-22 15:31:23 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'Q5OMib+GGlGuElOssf6lDOxqS+sgOS77Xjt1EBKX+qGLCSlMIgINJVJBNroZEJZV4CcLQT9if8oSYrAfJhZUv5caGjuDg9uVMxdSXUv3nj/ywQNjkzaYiC+dwVAIMWkeGlElPYdRNB8n+B7YVN2+SAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
