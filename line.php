011d5dba9afbb596240bb4ff22e72452e427a6be
popeye-api <70052083+popeye-api@users.noreply.github.com>
2020-09-01 15:28:34 +0700

    Update line.php

--- line.php
+++ line.php
@@ -28,6 +28,22 @@ if ( sizeof($request_array['events']) > 0 )
 	  	if($text == "CDMA" || $text == "cdma"){
 			$reply_message = '+1,-3,-1,-1';
 		}
+	   	if($text=="@บอท ขอรหัสนิสิตของผู้พัฒนา ส่งไปที่ https://linebot.kantit.com/stuid.php"){
+			$url = 'https://linebot.kantit.com/stuid.php';
+			$ch = curl_init($url);
+		     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		     curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+		     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+		     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		     $result = curl_exec($ch);
+		     curl_close($ch);   
+
+		     $obj = json_decode($result);
+
+		     //$reply_message = $result;
+		     $reply_message = 'ผลการบันทึกข้อมูล '. $obj->{'status'}."\n".' และ '. $obj->{'data'};
+		}
 	       if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
 		     $url = 'https://covid19.th-stat.com/api/open/today';
 		     $ch = curl_init($url);
